6d058be288e8f3be9a471fb08141aea4f6245f21
Sylvan <senguo@Sylvans-MacBook-Pro.local>
2023-03-07 18:17:57 -0800

    added login system using cookies

---
+++ Pages/Account/Login.cshtml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using MySqlConnector;
+using System.Data;
+
+namespace WebAppMysql.Pages.Account
+{
+	public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential credential { get; set; }
+
+
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid) return Page();
+            string uname = string.Empty;
+            string pw = string.Empty;
+            try
+            {
+                DotNetEnv.Env.Load();
+                var password = Environment.GetEnvironmentVariable("PASSWORD");
+                var server = Environment.GetEnvironmentVariable("SERVER");
+                var user = Environment.GetEnvironmentVariable("USER");
+                var database = Environment.GetEnvironmentVariable("DATABASE");
+                String connectionString = $"server={server};user={user};password={password};database={database}";
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Users WHERE username = @username";
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@username", credential.UserName);
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    //Console.WriteLine(reader.GetString(1));
+                                    //Console.WriteLine(reader.GetString(2));
+                                    uname = reader.GetString(1);
+                                    pw = reader.GetString(2);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //verify the credential
+            Console.WriteLine(uname, pw);
+            if (credential.UserName == uname && BCrypt.Net.BCrypt.Verify(credential.Password, pw))
+            {
+                //Console.WriteLine("Successfully verified");
+                //creating the security context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@email.com")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+                return RedirectToPage("/Index");
+            }
+            return Page();
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name ="User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
-﻿using MySqlConnector;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MySqlConnector;
 var builder = WebApplication.CreateBuilder(args);
 
 //register database connection
 builder.Services.AddTransient<MySqlConnection>(_ => new MySqlConnection(builder.Configuration.GetConnectionString("Default")));
+
+//register authentication service
+builder.Services.AddAuthentication("MyCookieAuth").AddCookie("MyCookieAuth", options =>
+{
+    options.Cookie.Name = "MyCookieAuth";
+
+});
+
+//builder.Services.AddAuthentication().AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, OptionsBuilderConfigurationExtensions =>
+//{
+
+
+
+//});
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -18,6 +34,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//inject authentication service to the middleware pipeline
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
